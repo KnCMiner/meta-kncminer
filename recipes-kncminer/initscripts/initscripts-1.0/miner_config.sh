e1235754a3df1721a56e7b09b0c70e67a38113fb
Erik Källberg <erikkallberg@bredband.net>
2013-09-26 14:13:25 +0200

    Fixed minor bug.

--- recipes-kncminer/initscripts/initscripts-1.0/miner_config.sh
+++ recipes-kncminer/initscripts/initscripts-1.0/miner_config.sh
@@ -12,16 +12,16 @@ r_mgmt=`cat /config/cgminer.conf|grep '"api-listen" :' | awk '{print $3}'`
 
 if [ "$r_mgmt" = "true" ] ; then
     sed  '
-s&#%#Pool_url#%#&'$url'&g
-s&#%#Account#%#&'$account'&g
-s&#%#Password#%#&'$password'&g
+s&#%#Pool_url#%#&'"$url"'&g
+s&#%#Account#%#&'"$account"'&g
+s&#%#Password#%#&'"$password"'&g
 s&#%#r_mgmt_on_checked#%#&checked&g
 s&#%#r_mgmt_off_checked#%#&&g' < /www/tmpl/miner_setting.html_tmpl > /www/pages/miner_setting.html
 else
     sed  '
-s&#%#Pool_url#%#&'$url'&g
-s&#%#Account#%#&'$account'&g
-s&#%#Password#%#&'$password'&g
+s&#%#Pool_url#%#&'"$url"'&g
+s&#%#Account#%#&'"$account"'&g
+s&#%#Password#%#&'"$password"'&g
 s&#%#r_mgmt_on_checked#%#&&g
 s&#%#r_mgmt_off_checked#%#&checked&g' < /www/tmpl/miner_setting.html_tmpl > /www/pages/miner_setting.html
 fi
