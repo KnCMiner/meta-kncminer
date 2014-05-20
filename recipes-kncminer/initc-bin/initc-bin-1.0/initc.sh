cbfa4c64044e5e5274471921316f1d621c246428
Vitalii Demianets <vitalii@orsoc.se>
2014-05-20 16:21:58 +0200

    Special workaround while 8-GE test is broken  (the test is run with only 4 DC/DCs enabled)

--- recipes-kncminer/initc-bin/initc-bin-1.0/initc.sh
+++ recipes-kncminer/initc-bin/initc-bin-1.0/initc.sh
@@ -55,7 +55,7 @@ if [[ $exit_code = 0 ]] ; then
         echo high > /sys/class/gpio/gpio70/direction
         echo low > /sys/class/gpio/gpio71/direction
 
-        /usr/bin/screen -d -m ./asic_test -r B -m G 0
+        /usr/bin/screen -d -m ./asic_test -r A -m G 0
 else
         # Turn ON red, Turn OFF green
         echo low > /sys/class/gpio/gpio70/direction
